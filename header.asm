ce94422b0f59455cc092d68c5cccf29a6ee3b7c8
Sean Zhang <seanzhang1207@163.com>
2016-04-29 17:06:43 +0800

    edit
    edited lots of stuff.
    cleaned up code etc.

--- header.asm
+++ header.asm
@@ -1,15 +1,15 @@
 global _loader                          ; Make entry point visible to linker.
-extern main                             ; main is defined elsewhere
+extern init                             ; main is defined elsewhere
 
 bits 32
- 
+
 ; setting up the Multiboot header - see GRUB docs for details
 MODULEALIGN equ  1<<0                   ; align loaded modules on page boundaries
 MEMINFO     equ  1<<1                   ; provide memory map
 FLAGS       equ  MODULEALIGN | MEMINFO  ; this is the Multiboot 'flag' field
 MAGIC       equ    0x1BADB002           ; 'magic number' lets bootloader find the header
 CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum required
- 
+
 ; This is the virtual base address of kernel space. It must be used to convert virtual
 ; addresses into physical addresses until paging is enabled. Note that this is not
 ; the virtual address where the kernel image itself is loaded -- just the amount that must
@@ -28,20 +28,20 @@ BootPageDirectory:
     ; bit 0: P  The kernel page is present.
     ; This entry must be here -- otherwise the kernel will crash immediately after paging is
     ; enabled because it can't fetch the next instruction! It's ok to unmap this page later.
-    dd BootPageTable - KERNEL_VIRTUAL_BASE + 3
+    dd BootPageTable - KERNEL_VIRTUAL_BASE + 7
     times (KERNEL_PAGE_NUMBER - 1) dd 0                 ; page tables before kernel space.
     ; This page table entry defines a 4MB mem range containing the kernel.
-    dd BootPageTable - KERNEL_VIRTUAL_BASE + 3
+    dd BootPageTable - KERNEL_VIRTUAL_BASE + 7
     times (1024 - KERNEL_PAGE_NUMBER - 2) dd 0  ; page tables after the kernel image.
-    dd BootPageDirectory - KERNEL_VIRTUAL_BASE + 3  ; map last entry of page dir to itself.
+    dd BootPageDirectory - KERNEL_VIRTUAL_BASE + 7  ; map last entry of page dir to itself.
 
 BootPageTable:
-    %assign i 0 
+    %assign i 0
     %rep    1024
-        dd (i * 4096) | 3
-    %assign i i+1 
+        dd (i * 4096) | 7
+    %assign i i+1
     %endrep
- 
+
 section .Multiboot
 MultiBootHeader:
     dd MAGIC
@@ -51,24 +51,24 @@ MultiBootHeader:
 
 section .boot
 align 4
- 
+
 ; reserve initial kernel stack space -- that's 16k.
 STACKSIZE equ 0x4000
- 
+
 ; setting up entry point for linker
- 
+
 _loader:
     ; NOTE: Until paging is set up, the code must be position-independent and use physical
     ; addresses, not virtual ones!
-    cli 
+    cli
 
     mov ecx, (BootPageDirectory - KERNEL_VIRTUAL_BASE)
     mov cr3, ecx                                        ; Load Page Directory Base Register.
- 
+
     mov ecx, cr0
     or ecx, 0x80000000                          ; Set PG bit in CR0 to enable paging.
     mov cr0, ecx
- 
+
     ; Start fetching instructions in kernel space.
     ; Since eip at this point holds the physical address of this command (approximately 0x00100000)
     ; we need to do a long jump to the correct virtual address of StartInHigherHalf which is
@@ -99,103 +99,28 @@ _reload_CS:
     mov fs, ax
     mov gs, ax
     mov ss, ax
- 
+
     mov esp, stack+STACKSIZE            ; set up the stack
+
+    ;Check if CPUID is supported
+    pushfd                               ;Save EFLAGS
+    pushfd                               ;Store EFLAGS
+    xor dword [esp],0x00200000           ;Invert the ID bit in stored EFLAGS
+    popfd                                ;Load stored EFLAGS (with ID bit inverted)
+    pushfd                               ;Store EFLAGS again (ID bit may or may not be inverted)
+    pop eax                              ;eax = modified EFLAGS (ID bit may or may not be inverted)
+    xor eax,[esp]                        ;eax = whichever bits were changed
+    popfd                                ;Restore original EFLAGS
+    and eax,0x00200000                   ;eax = zero if ID bit can't be changed, else non-zero
+    push eax
 
     add ebx, KERNEL_VIRTUAL_BASE        ; add kernel base to multiboot info structure
     push ebx
 
-    ; now that we have multiboot info pushed onto stack, 
+    ; now that we have multiboot info pushed onto stack,
     ; we need to do some chores before calling the kernel.
 
     ; Reprogram the 8259 PIC chip
-    ; IRQ 0x00~0x0f vectors are 0x20~0x2f
-
-    MPIC_C equ 0x20
-    MPIC_D equ 0x21
-    SPIC_C equ 0xa0
-    SPIC_D equ 0xa1
-
-    in al,MPIC_D
-    mov bh,al
-    in al,SPIC_D
-    mov bl,al
-    mov al,0x11
-    out MPIC_C,al   
-    jmp $+2     
-    jmp $+2
-    mov al,0x11
-    out SPIC_C,al
-    jmp $+2
-    jmp $+2
-    mov al,0x20
-    out MPIC_D,al
-    jmp $+2
-    jmp $+2
-    mov al,0x28
-    out SPIC_D,al
-    jmp $+2
-    jmp $+2
-    mov al,4
-    out MPIC_D,al
-    jmp $+2
-    jmp $+2
-    mov al,2
-    out SPIC_D,al
-    jmp $+2
-    jmp $+2
-    mov al,1
-    out MPIC_D,al
-    jmp $+2
-    jmp $+2
-    out SPIC_D,al
-    jmp $+2
-    jmp $+2
-    mov al,bh
-    out MPIC_D,al
-    jmp $+2
-    jmp $+2
-    mov al,bl
-    out SPIC_D,al
-
-    ; setup a dummy IDT for future function installation. 
-    mov ecx, 255
-_idt_loop:
-    push dword 0x8e00
-    push dword 0x8
-    push dword dummy_isr
-    push ecx
-    call set_idt_entry
-    pop eax
-    pop eax
-    pop eax
-    pop eax
-    loop _idt_loop
-
-
-; ================[TEST CODE BELOW]================
-    push dword 0x8e00
-    push dword 0x8
-    push dword keyboard_isr
-    push dword 0x21
-    call set_idt_entry
-    pop eax
-    pop eax
-    pop eax
-    pop eax
-
-    push dword 0x8e00
-    push dword 0x8
-    push dword sys_call
-    push dword 0x99
-    call set_idt_entry
-    pop eax
-    pop eax
-    pop eax
-    pop eax
-; ================[TEST CODE ABOVE]================
-
-    lidt [IDTR]
 
 ; ================[TEST CODE BELOW]================
     mov eax, TEST_TSS
@@ -212,102 +137,87 @@ _idt_loop:
     shr eax, 24
     mov [ebx], al
 ; ================[TEST CODE ABOVE]================
- 
-    call  main                  ; call kernel proper
+
+    call init
 ; ================[TEST CODE BELOW]================
     ;call dummy_isr
+    lidt [IDTR]
     mov eax, 0x2b
     ltr ax
+
     sti
-    call goto_usermode
+
+    mov ecx, user_stack
+    mov edx, test_user
+    mov ax, 0x23
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    sysexit
+
 ; ================[TEST CODE ABOVE]================
-    jmp $                          ; halt machine should kernel return
+
+    jmp $                          ; halt
+
+global sysenter_call
+test_user:
+    mov ecx, esp
+    mov edx, _user_ret
+    sysenter
+_user_ret:
+    jmp test_user
+
+sysenter_call:
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    sti
+    sysexit
+
+sysexit_halt:
+    jmp $
 
 
-; ================[TEST CODE BELOW]================
-extern pchar
-global goto_usermode
+; Intel syntax
 
-dummy_isr:
-    pushad
-    mov dx,0x20
-    mov al,0x20
-    out dx,al
-    popad
-    iret
+locked:                      ; The lock variable. 1 = locked, 0 = unlocked.
+     dd      0
 
-extern key
+spin_lock:
+     mov     eax, 1          ; Set the EAX register to 1.
 
-keyboard_isr:
-    pushad
-    mov eax, 0
-    in al, 0x60
-    push eax
-    call key
-    pop eax
-    mov dx,0x20
-    mov al,0x20
-    out dx,al
-    popad
-    iret
+     xchg    eax, [locked]   ; Atomically swap the EAX register with
+                             ;  the lock variable.
+                             ; This will always store 1 to the lock, leaving
+                             ;  the previous value in the EAX register.
 
-extern syscall_print
-sys_call:
-    pushad
-    call syscall_print
-    mov dx,0x20
-    mov al,0x20
-    out dx,al
-    popad
-    iret
+     test    eax, eax        ; Test EAX with itself. Among other things, this will
+                             ;  set the processor's Zero Flag if EAX is 0.
+                             ; If EAX is 0, then the lock was unlocked and
+                             ;  we just locked it.
+                             ; Otherwise, EAX is 1 and we didn't acquire the lock.
 
-goto_usermode:
-    mov ax,0x23
-    mov ds,ax
-    mov es,ax 
-    mov fs,ax 
-    mov gs,ax ;we don't need to worry about SS. it's handled by iret
- 
-    mov eax,esp
-    push 0x23 ;user data segment with bottom 2 bits set for ring 3
-    push eax ;push our current stack just for the heck of it
-    pushf
-    push 0x1B; ;user code segment with bottom 2 bits set for ring 3
-    push usermode ;may need to remove the _ for this to work right 
-    iret
+     jnz     spin_lock       ; Jump back to the MOV instruction if the Zero Flag is
+                             ;  not set; the lock was previously locked, and so
+                             ; we need to spin until it becomes unlocked.
 
-usermode:
-    int 0x99
-    jmp usermode
+     ret                     ; The lock has been acquired, return to the calling
+                             ;  function.
+
+spin_unlock:
+     mov     eax, 0          ; Set the EAX register to 0.
+
+     xchg    eax, [locked]   ; Atomically swap the EAX register with
+                             ;  the lock variable.
+
+     ret                     ; The lock has been released.
 
 
 
-global set_idt_entry
-set_idt_entry:
-    push ebp
-    mov ebp,esp
-    push eax
-    push ebx
-    push ecx
-    push edx
-    mov eax,[ebp+8]
-    shl eax,3
-    add eax,IDT
-    mov ebx,[ebp+12]
-    mov ecx,[ebp+16]
-    mov edx,[ebp+20]
-    mov [eax],bx
-    mov [eax+2],cx
-    mov [eax+4],dx
-    shr ebx,16
-    mov [eax+6],bx
-    pop edx
-    pop ecx
-    pop ebx
-    pop eax
-    pop ebp
-    ret
-; ================[TEST CODE ABOVE]================
+
 
 align 4
 
@@ -373,7 +283,7 @@ _3:
 
 
 GDT_END:
-
+global IDT
 align 8
 IDT:
     times 512 dd 0  ; 256 entries, each 8 bytes long.
@@ -385,7 +295,7 @@ TEST_TSS:
 esp0:
     dd tssstack + 4096      ; The stack pointer to load when we change to kernel mode.
     dd 0x10        ; The stack segment to load when we change to kernel mode.
-    dd 0       ; everything below here is unusued now.. 
+    dd 0       ; everything below here is unusued now..
     dd 0
     dd 0
     dd 0
@@ -410,7 +320,7 @@ esp0:
     dd 0
 TEST_TSS_END:
 ; ================[TEST CODE ABOVE]================
- 
+
 section .bss
 align 32
 stack:
@@ -419,10 +329,12 @@ stack:
 ; ================[TEST CODE BELOW]================
 tssstack:
     resb STACKSIZE      ; reserve 16k stack on a uint64_t boundary for tss
+
+global syscall_stack
+
+syscall_stack:
+    resb STACKSIZE      ; reserve 16k stack on a uint64_t boundary for tss
+
+user_stack:
+    resb STACKSIZE
 ; ================[TEST CODE ABOVE]================
-
-
-
-
-
-
